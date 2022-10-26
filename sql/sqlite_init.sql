a4b23f19eefc4686a9094de6fc7870d2b3535037
Offerel <offerel@noreply.codeberg.org>
2022-10-26 12:42:19 +0200

    „sql/sqlite_init.sql“ ändern

--- sql/sqlite_init.sql
+++ sql/sqlite_init.sql
@@ -1,5 +1,5 @@
 -- Create users table
-CREATE TABLE "users" (
+CREATE TABLE `users` (
 	`userID`	INTEGER NOT NULL UNIQUE,
 	`userName`	TEXT NOT NULL UNIQUE,
 	`userType`	INTEGER NOT NULL,
@@ -29,14 +29,14 @@ CREATE TABLE `bookmarks` (
 
 -- Create clients table
 CREATE TABLE `clients` (
-	`cid`	TEXT NOT NULL UNIQUE,
+	`cid`	TEXT NOT NULL DEFAULT NULL UNIQUE,
 	`cname`	TEXT,
 	`ctype`	TEXT NOT NULL,
 	`uid`	INTEGER NOT NULL,
 	`lastseen`	TEXT NOT NULL DEFAULT 0,
 	`fs`	INTEGER NOT NULL DEFAULT 0,
-	FOREIGN KEY(`uid`) REFERENCES `users`(`userID`) ON DELETE CASCADE,
-	PRIMARY KEY(`cid`)
+	PRIMARY KEY(`cid`),
+	FOREIGN KEY(`uid`) REFERENCES `users`(`userID`) ON DELETE CASCADE
 );
 
 -- Create notifications table
@@ -45,37 +45,41 @@ CREATE TABLE `notifications` (
 	`title`	varchar(250) NOT NULL,
 	`message`	TEXT NOT NULL,
 	`ntime`	varchar(250) NOT NULL DEFAULT NULL,
-	`client`	TEXT NOT NULL DEFAULT 0,
+	`client`	TEXT DEFAULT NULL,
 	`nloop`	INTEGER NOT NULL DEFAULT 1,
 	`publish_date`	varchar(250) NOT NULL,
 	`userID`	INTEGER NOT NULL,
 	PRIMARY KEY(`id`),
-	FOREIGN KEY(`userID`) REFERENCES `users`(`userID`) ON DELETE CASCADE
+	FOREIGN KEY(`userID`) REFERENCES `users`(`userID`) ON DELETE CASCADE,
+	FOREIGN KEY(`client`) REFERENCES `clients`(`cid`) ON DELETE SET NULL
 );
 
 -- Create reset table
-CREATE TABLE IF NOT EXISTS `reset` (
+CREATE TABLE `reset` (
 	`tokenID`	INTEGER NOT NULL UNIQUE,
 	`userID`	INTEGER NOT NULL,
 	`tokenTime`	VARCHAR(255) NOT NULL,
 	`token`	VARCHAR(255) NOT NULL UNIQUE,
+	FOREIGN KEY(`userID`) REFERENCES `users`(`userID`) ON DELETE CASCADE,
 	PRIMARY KEY(`tokenID` AUTOINCREMENT)
 );
 
 -- Create system table
-CREATE TABLE IF NOT EXISTS `system` (
+CREATE TABLE `system` (
 	`app_version`	varchar(10),
 	`db_version`	varchar(10),
 	`updated`	varchar(250)
 );
 
 -- CREATE tokens table
-CREATE TABLE IF NOT EXISTS "auth_token" (
-	`tID`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+CREATE TABLE `auth_token` (
+	`tID`	INTEGER UNIQUE,
 	`userName`	TEXT NOT NULL,
-	`pHash`	VARCHAR(255) NOT NULL,
+	`pHash`	VARCHAR(255),
 	`tHash`	VARCHAR(255) NOT NULL,
-	`exDate`	VARCHAR(255) NOT NULL
+	`exDate`	VARCHAR(255) NOT NULL,
+	FOREIGN KEY(`userName`) REFERENCES `users`(`userName`) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY(`tID` AUTOINCREMENT)
 );
 
 -- CREATE ctokens table
@@ -86,7 +90,9 @@ CREATE TABLE `c_token` (
 	`exDate`	VARCHAR(255) NOT NULL,
 	`userID`	INTEGER NOT NULL,
 	`cInfo`	TEXT,
-	PRIMARY KEY(`tID` AUTOINCREMENT)
+	FOREIGN KEY(`userID`) REFERENCES `users`(`userID`) ON DELETE CASCADE,
+	PRIMARY KEY(`tID` AUTOINCREMENT),
+	FOREIGN KEY(`cid`) REFERENCES `clients`(`cid`) ON DELETE CASCADE
 );
 
 -- Create index
@@ -95,49 +101,10 @@ CREATE INDEX `i2` ON `users` ( `userID`);
 CREATE INDEX `i3` ON `clients` (`cid`);
 
 -- Create triggers
-CREATE TRIGGER IF NOT EXISTS `on_delete_set_default`
-	AFTER DELETE ON `clients`
-BEGIN
-	UPDATE `notifications` SET `client` = 0 WHERE `client` = old.cid;
-END;
-
-CREATE TRIGGER IF NOT EXISTS`delete_userclients `
-	AFTER DELETE ON `users`
-	FOR EACH ROW
-BEGIN
-	DELETE FROM `clients` WHERE `uid` = OLD.userID;
-END;
-
-CREATE TRIGGER IF NOT EXISTS `delete_usermarks`
-	AFTER DELETE ON `users`
-	FOR EACH ROW
-BEGIN
-	DELETE FROM `bookmarks` WHERE `userID` = OLD.userID;
-END;
-
 CREATE TRIGGER IF NOT EXISTS `update_tokenchange`
 	UPDATE ON `auth_token`
 BEGIN
 	DELETE FROM `auth_token` WHERE `exDate` < strftime('%s') OR expired <> 0;
-END;
-
-CREATE TRIGGER IF NOT EXISTS `delete_usertokens`
-	AFTER DELETE ON `users`
-BEGIN
-	DELETE FROM `auth_token` WHERE `userName` = OLD.userName;
-END;
-
-CREATE TRIGGER IF NOT EXISTS `update_usertoken`
-	UPDATE OF `userName`, `userHash`, `userType` ON `users`
-BEGIN
-	DELETE FROM `auth_token` WHERE `userName` = OLD.userName;
-END;
-
-CREATE TRIGGER IF NOT EXISTS  `delete_clientokens`
-	AFTER DELETE ON `clients`
-	FOR EACH ROW
-BEGIN
-	DELETE FROM `c_token` WHERE `cid` = OLD.cid;
 END;
 
 CREATE TRIGGER `delete_subbm`
